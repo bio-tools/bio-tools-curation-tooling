b71accebc27cf2b6213863436460db04c3063bea
Ana Isabel da Silva Mendes <48804086+anitamnd@users.noreply.github.com>
2024-03-30 11:53:39 +0100

    generate csv file for curation

---
+++ csv_utils.py
@@ -0,0 +1,53 @@
+import re
+import csv
+
+def check_date(pub2tools_file):
+    '''Function to check the date of the pub2tools file'''
+    log_file = open(pub2tools_file, 'r')
+    textfile = log_file.read()
+    log_file.close()
+    date = re.findall("--month (\d+)-(\d+)", textfile)
+
+    return date[0]
+
+def filter_tools(tools, to_curate, filter_condition=lambda x: True):
+    """Filters tools based on a condition and curate limit."""
+    filtered_tools = [tool for tool in tools if filter_condition(tool)]
+    if to_curate != 'all':
+        filtered_tools = filtered_tools[:int(to_curate)]
+    return filtered_tools
+
+def generate_file_name(prefix, file_date):
+    """Generates a file name based on a prefix and a date tuple."""
+    year, month = file_date[0], file_date[1]
+    return f"{prefix}_{year}_{month}.csv"
+
+def generate_csv(tools, to_curate, file_settings, include_preprints=True):
+    """Generates a CSV file for tools, separating or including preprints based on parameters."""
+    file_date = check_date(file_settings['pub2tools_log'])
+    file_name = generate_file_name('pub2tools', file_date)
+    
+    # Filter tools based on 'is_preprint' and 'to_curate'
+    publications = filter_tools(tools, to_curate, lambda x: not x['is_preprint'])
+    preprints = []
+
+    if include_preprints:
+        preprints = filter_tools(tools, to_curate, lambda x: x['is_preprint'])
+
+    print(f"Writing {to_curate} files: {len(publications)} publications and {len(preprints)} preprints to {file_name}")
+    
+    # Combine lists if including preprints, else just use publications
+    combined_tools = publications + (['PREPRINTS'] if preprints else []) + preprints
+    
+    # Write to CSV, handle 'PREPRINTS' marker
+    with open(file_name, 'w', newline='') as fileobj:
+        writerobj = csv.writer(fileobj)
+        writerobj.writerow(['tool_link', 'tool_name', 'homepage', 'publication_link'])
+        for tool in combined_tools:
+            if tool == 'PREPRINTS':
+                writerobj.writerow([tool])
+            else:
+                writerobj.writerow([tool['tool_link'], tool['name'], tool['homepage'], tool['publication_link']])
+    
+    leftover_tools = [tool for tool in tools if tool not in publications and tool not in preprints]
+    return publications + preprints, leftover_tools

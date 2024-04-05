b4b913457c752524fe2f9f354bd1b6c98bed6e5c
AlSzmigiel <aleksandraszmigiel58@gmail.com>
2024-04-05 13:43:13 +0200

    delete separate preprints

--- utils/json_utils.py
+++ utils/json_utils.py
@@ -19,28 +19,12 @@ def save_tools_to_json(tools, json_path):
         logging.error(f"Error saving tools to JSON {json_path}: {e}")
 
 
-def generate_json(tools, file_date, separate_preprints=True):
-    output_dir = f"process_scrap_{file_date[0]}_{file_date[1]}"
+def generate_json(tools, file_date):
+    output_dir = os.path.join(os.getcwd(), "data")
     os.makedirs(output_dir, exist_ok=True)
-
-    base_file_name = f"low_tools_{file_date[0]}_{file_date[1]}"
-
-    if separate_preprints:
-        # Separate tools based on 'is_preprint' attribute
-        preprints = [tool for tool in tools if tool['is_preprint']]
-        publications = [tool for tool in tools if not tool['is_preprint']]
-
-        # JSON for preprints
-        json_prp_file_name = os.path.join(output_dir, f"{base_file_name}_prp.json")
-        save_tools_to_json(preprints, json_prp_file_name)
-
-        # JSON for publications
-        json_pub_file_name = os.path.join(output_dir, f"{base_file_name}_pub.json")
-        save_tools_to_json(publications, json_pub_file_name)
-
-    else:
-        # Generate a single JSON file for all tools
-        json_all_file_name = os.path.join(output_dir, f"{base_file_name}.json")
-        save_tools_to_json(tools, json_all_file_name)
+    output_name = f"low_tools_{file_date[0]}_{file_date[1]}"
+    
+    json_all_file_name = os.path.join(output_dir, f"{output_name}.json")
+    save_tools_to_json(tools, json_all_file_name)
 
     print(f"JSON files generated in {output_dir}.")
